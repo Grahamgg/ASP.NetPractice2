5ca9c2bc323ae0b51ed0f95f4b43cd3e2ded2087
Graham Galloway <Grahamgalloway0@gmail.com>
2023-07-24 07:51:26 -0400

    Finishe Project

--- NathansCRUDWebsite/Controllers/ProductController.cs
+++ NathansCRUDWebsite/Controllers/ProductController.cs
@@ -27,6 +27,37 @@ namespace NathansCRUDWebsite.Controllers
 
             return View(product);
         }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+
+            return View(prod);
+        }
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+
 
 
     }
